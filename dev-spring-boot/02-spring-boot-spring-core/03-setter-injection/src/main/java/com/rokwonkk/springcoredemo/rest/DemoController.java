81d4f7d89bf7a60648f984360ac1a82275be3374
로건리 <jirisan8147@gmail.com>
2024-05-25 23:11:11 +0900

    use setter injection

---
+++ dev-spring-boot/02-spring-boot-spring-core/03-setter-injection/src/main/java/com/rokwonkk/springcoredemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.rokwonkk.springcoredemo.rest;
+
+import com.rokwonkk.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String dailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
