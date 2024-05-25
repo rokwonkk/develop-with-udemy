584d2a21d5edc6cbfe40bad9c8ddde90168a65ea
로건리 <jirisan8147@gmail.com>
2024-05-26 00:56:20 +0900

    use qualifiers

---
+++ dev-spring-boot/02-spring-boot-spring-core/04-qualifiers/src/main/java/com/rokwonkk/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.rokwonkk.springcoredemo.rest;
+
+import com.rokwonkk.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    @Autowired
+    public void DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String dailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
