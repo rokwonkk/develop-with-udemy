c9b336b16acab87c5050117fe6f11a30a0b715e6
로건리 <jirisan8147@gmail.com>
2024-05-29 00:27:56 +0900

    use bean-scopes

---
+++ dev-spring-boot/02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/rokwonkk/springcoredemo/rest/DemoController.java
@@ -0,0 +1,33 @@
+package com.rokwonkk.springcoredemo.rest;
+
+import com.rokwonkk.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+    private Coach anotherCoach;
+
+    @Autowired
+    public void DemoController(@Qualifier("cricketCoach") Coach theCoach,
+                               @Qualifier("cricketCoach") Coach theAnotherCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String dailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
+    }
+}
