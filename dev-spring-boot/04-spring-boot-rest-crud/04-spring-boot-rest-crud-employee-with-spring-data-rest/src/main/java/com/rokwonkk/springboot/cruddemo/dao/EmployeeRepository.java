ca3cadefb3b528cf43c858dcf11c72a9499ffab1
로건리 <jirisan8147@gmail.com>
2024-06-05 13:34:46 +0900

    use rest crud spring-data-rest

---
+++ dev-spring-boot/04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/rokwonkk/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.rokwonkk.springboot.cruddemo.dao;
+
+import com.rokwonkk.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL
+
+}
