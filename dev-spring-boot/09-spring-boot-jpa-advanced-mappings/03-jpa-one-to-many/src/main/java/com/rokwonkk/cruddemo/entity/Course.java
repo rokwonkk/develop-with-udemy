6fe346ae818b93fb3d91e045d621a840dfff7d7b
로건리 <jirisan8147@gmail.com>
2024-06-14 01:48:07 +0900

    use many to one join

--- dev-spring-boot/09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/rokwonkk/cruddemo/entity/Course.java
+++ dev-spring-boot/09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/rokwonkk/cruddemo/entity/Course.java
@@ -1,7 +1,6 @@
 package com.rokwonkk.cruddemo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="course")
@@ -17,15 +16,51 @@ public class Course {
 
     // annotate fields
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private int id;
 
+    @Column(name="title")
     private String title;
 
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name="instructor_id")
     private Instructor instructor;
 
     public Course() {
     }
 
+    public Course(String title) {
+        this.title = title;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public Course(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
 }
