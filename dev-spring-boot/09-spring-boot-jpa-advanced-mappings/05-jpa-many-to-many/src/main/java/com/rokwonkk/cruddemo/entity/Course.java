301ad2ccb7e32706798214c734ad10458848cdc4
로건리 <jirisan8147@gmail.com>
2024-06-26 23:30:47 +0900

    use mapping course and student

--- dev-spring-boot/09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/rokwonkk/cruddemo/entity/Course.java
+++ dev-spring-boot/09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/rokwonkk/cruddemo/entity/Course.java
@@ -25,6 +25,15 @@ public class Course {
     @JoinColumn(name="course_id")
     private List<Review> reviews;
 
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name="course_student",
+            joinColumns = @JoinColumn(name="course_id"),
+            inverseJoinColumns = @JoinColumn(name="student_id")
+    )
+    private List<Student> students;
+
     public Course() {
     }
 
@@ -75,6 +84,21 @@ public class Course {
             reviews = new ArrayList<>();
         }
         reviews.add(review);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public void addStudent(Student student) {
+        if (students == null) {
+            students = new ArrayList<>();
+        }
+        students.add(student);
     }
 
     @Override
